Neoteric-OS/android_frameworks_base

Add checks to ensure SPIs are not reused
This change adds an additional check in CheckIpsecConfig to prevent
users from using the same SPI twice. This allows for a more granular
error message.

Bug: 70642141
Test: Tests added in IpSecServiceParameterizedTest
Change-Id: I9621fb05c6b162bd8ae8db4ac1e64feaa9d0ac73

services/core/java/com/android/server/IpSecService.java
@@ -535,6 +535,10 @@ public class IpSecService extends IIpSecService.Stub {
             mConfig = config;
             mSpis = spis;
             mSocket = socket;
+
+            for (SpiRecord spiRecord : spis) {
+                spiRecord.setOwnedByTransform();
+            }
         }
 
         public IpSecConfig getConfig() {
@@ -659,6 +663,10 @@ public class IpSecService extends IIpSecService.Stub {
             }
 
             mOwnedByTransform = true;
+        }
+
+        public boolean getOwnedByTransform() {
+            return mOwnedByTransform;
         }
 
         @Override
@@ -1044,7 +1052,14 @@ public class IpSecService extends IIpSecService.Stub {
             }
 
             // Retrieve SPI record; will throw IllegalArgumentException if not found
-            userRecord.mSpiRecords.getResourceOrThrow(config.getSpiResourceId(direction));
+            SpiRecord record =
+                    userRecord.mSpiRecords.getResourceOrThrow(config.getSpiResourceId(direction));
+
+            // Check to ensure that SPI has not already been used.
+            if (record.getOwnedByTransform()) {
+                throw new IllegalArgumentException(
+                        "SPI already in use; cannot be used in new Transforms");
+            }
         }
     }
 

Claymore1297/frameworks_base

services/core/java/com/android/server/IpSecService.java
@@ -571,6 +571,8 @@ public class IpSecService extends IIpSecService.Stub {
             mConfig = config;
             mSpi = spi;
             mSocket = socket;
+
+            spi.setOwnedByTransform();
         }
 
         public IpSecConfig getConfig() {
@@ -651,16 +653,6 @@ public class IpSecService extends IIpSecService.Stub {
         /** always guarded by IpSecService#this */
         @Override
         public void freeUnderlyingResources() {
-            if (mOwnedByTransform) {
-                Log.d(TAG, "Cannot release Spi " + mSpi + ": Currently locked by a Transform");
-                // Because SPIs are "handed off" to transform, objects, they should never be
-                // freed from the SpiRecord once used in a transform. (They refer to the same SA,
-                // thus ownership and responsibility for freeing these resources passes to the
-                // Transform object). Thus, we should let the user free them without penalty once
-                // they are applied in a Transform object.
-                return;
-            }
-
             try {
                 mSrvConfig
                         .getNetdInstance()
@@ -692,6 +684,10 @@ public class IpSecService extends IIpSecService.Stub {
             }
 
             mOwnedByTransform = true;
+        }
+
+        public boolean getOwnedByTransform() {
+            return mOwnedByTransform;
         }
 
         @Override
@@ -1106,6 +1102,11 @@ public class IpSecService extends IIpSecService.Stub {
 
         // Retrieve SPI record; will throw IllegalArgumentException if not found
         SpiRecord s = userRecord.mSpiRecords.getResourceOrThrow(config.getSpiResourceId());
+
+        // Check to ensure that SPI has not already been used.
+        if (s.getOwnedByTransform()) {
+            throw new IllegalStateException("SPI already in use; cannot be used in new Transforms");
+        }
 
         // If no remote address is supplied, then use one from the SPI.
         if (TextUtils.isEmpty(config.getDestinationAddress())) {