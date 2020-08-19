Neoteric-OS/android_frameworks_base

IorapForwardingService: Suppress error log
Bug: 160381575
Test: make
Change-Id: Iee7506205edc2863c76d0dae0568297565def3df

startop/iorap/src/com/google/android/startop/iorap/IorapForwardingService.java
@@ -386,7 +386,7 @@ public class IorapForwardingService extends SystemService {
             throw new AssertionError("unexpected remote error", t);
         } else {
             // Log to wtf which gets sent to dropbox, and in system_server this does not crash.
-            Log.wtf(TAG, t);
+            Log.w(TAG, t.getMessage());
         }
     }
 }

Claymore1297/frameworks_base

startop/iorap/src/com/google/android/startop/iorap/IorapForwardingService.java
@@ -96,7 +96,7 @@ public class IorapForwardingService extends SystemService {
         try {
             iorap = IIorap.Stub.asInterface(ServiceManager.getServiceOrThrow("iorapd"));
         } catch (ServiceManager.ServiceNotFoundException e) {
-            handleRemoteError(e);
+            Log.w(TAG, e.getMessage());
             return null;
         }
 