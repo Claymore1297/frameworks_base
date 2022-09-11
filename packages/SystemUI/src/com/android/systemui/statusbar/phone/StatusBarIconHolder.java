68b2fa9b57c0bc1590c8dc528132a4e1fa817b6b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-11 16:59:36 +0530

    SystemUI: Refactor statusbar bluetooth icon
    Move it to its own layout like wifi and mobile,
    and separate the bluetooth connected and battery
    icons.
    
    This allows us to use bluetooth connected icon
    from icon packs, and also color the battery icon
    when its at low battery (in the next change).
    
    While we're at it, simplify the logic used to
    determine drawable from battery level, and also
    add icon for <10% battery.
    
    Change-Id: I5dd50d84497816484e1c33a7432a3d85c3a7f79d
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
@@ -23,6 +23,7 @@ import android.graphics.drawable.Icon;
 import android.os.UserHandle;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.systemui.statusbar.phone.PhoneStatusBarPolicy.BluetoothIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.MobileIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.WifiIconState;
@@ -72,11 +73,14 @@ public class StatusBarIconHolder {
     })
     @Retention(RetentionPolicy.SOURCE)
     @interface IconType {}
+    public static final int TYPE_BLUETOOTH = 4;
 
     private StatusBarIcon mIcon;
     private WifiIconState mWifiState;
     private MobileIconState mMobileState;
     private @IconType int mType = TYPE_ICON;
+    private BluetoothIconState mBluetoothState;
+
     private int mTag = 0;
 
     private StatusBarIconHolder() {
@@ -136,6 +140,14 @@ public class StatusBarIconHolder {
         return holder;
     }
 
+    /** */
+    public static StatusBarIconHolder fromBluetoothIconState(BluetoothIconState state) {
+        StatusBarIconHolder holder = new StatusBarIconHolder();
+        holder.mBluetoothState = state;
+        holder.mType = TYPE_BLUETOOTH;
+        return holder;
+    }
+
     /**
      * Creates a new StatusBarIconHolder from a CallIndicatorIconState.
      */
@@ -183,6 +195,15 @@ public class StatusBarIconHolder {
         mMobileState = state;
     }
 
+    @Nullable
+    public BluetoothIconState getBluetoothState() {
+        return mBluetoothState;
+    }
+
+    public void setBluetoothState(BluetoothIconState state) {
+        mBluetoothState = state;
+    }
+
     public boolean isVisible() {
         switch (mType) {
             case TYPE_ICON:
@@ -198,6 +219,8 @@ public class StatusBarIconHolder {
                 return true;
             default:
                 return true;
+            case TYPE_BLUETOOTH:
+                return mBluetoothState.visible;
         }
     }
 
@@ -223,6 +246,10 @@ public class StatusBarIconHolder {
             case TYPE_WIFI_NEW:
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // ignore setVisible.
+                break;
+
+            case TYPE_BLUETOOTH:
+                mBluetoothState.visible = visible;
                 break;
         }
     }
