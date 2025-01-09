6fb15fd92c2f96a9c3968a3eec4cf4aa1a7abbc9
Julian Veit <claymore1298@gmail.com>
2025-01-09 19:03:06 +0000

    Revert "[Spa] Upgrade AGP to 8.3.2"
    This reverts commit 0bc167dd08b8f474d34d186702345e898b205b2d.

--- packages/SettingsLib/Spa/spa/build.gradle.kts
+++ packages/SettingsLib/Spa/spa/build.gradle.kts
@@ -65,8 +65,7 @@ dependencies {
     api("androidx.lifecycle:lifecycle-runtime-compose")
     api("androidx.navigation:navigation-compose:2.8.0-beta01")
     api("com.github.PhilJay:MPAndroidChart:v3.1.0-alpha")
-    api("com.google.android.material:material:1.11.0")
-    api("androidx.graphics:graphics-shapes-android:1.0.1")
+    api("com.google.android.material:material:1.7.0-alpha03")
     debugApi("androidx.compose.ui:ui-tooling:$jetpackComposeVersion")
     implementation("com.airbnb.android:lottie-compose:5.2.0")
 
